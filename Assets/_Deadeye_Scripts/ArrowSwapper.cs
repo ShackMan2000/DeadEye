62e74ed402b6feba72592808836d3b7e5474494b
Julian <julianpieper@gmx.de>
2023-11-18 13:53:34 +0100

    clean up packages

--- Assets/_Deadeye_Scripts/ArrowSwapper.cs
+++ Assets/_Deadeye_Scripts/ArrowSwapper.cs
@@ -8,34 +8,36 @@ public class ArrowSwapper : MonoBehaviour
     public Material RedMaterial;
 
     public Material BlueMaterial;
-    
+
     // Start is called before the first frame update
     public void BLUE()
     {
-          foreach (GameObject obj in ArrowBody)
-                        {
-                            Renderer renderer = obj.GetComponent<Renderer>();
-                            
-                            if (renderer != null)
-                            {
-                                renderer.material = BlueMaterial;
-                            }
-                        }
-                    this.gameObject.tag = "BLUE";
+        foreach (GameObject obj in ArrowBody)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.material = BlueMaterial;
+            }
+        }
+
+        this.gameObject.tag = "BLUE";
     }
 
     // Update is called once per frame
     public void RED()
     {
-          foreach (GameObject obj in ArrowBody)
-                        {
-                            Renderer renderer = obj.GetComponent<Renderer>();
-                            
-                            if (renderer != null)
-                            {
-                                renderer.material = RedMaterial;
-                            }
-                        }
-                    this.gameObject.tag = "RED";
+        foreach (GameObject obj in ArrowBody)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.material = RedMaterial;
+            }
+        }
+
+        this.gameObject.tag = "RED";
     }
 }
