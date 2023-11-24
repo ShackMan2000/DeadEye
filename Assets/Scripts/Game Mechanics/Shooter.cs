308c2ea79a4713b143200a7deb3d4205f3e2a5bd
Julian <julianpieper@gmx.de>
2023-11-24 12:36:41 +0100

    updating oculus sdk

--- Assets/Scripts/Game Mechanics/Shooter.cs
+++ Assets/Scripts/Game Mechanics/Shooter.cs
@@ -3,23 +3,30 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    public static event Action<Vector3, Vector3> OnShootBulletStartToTarget = delegate { };
+    public static event Action<Vector3, Vector3> OnShootBulletStartToInfiniteDirection = delegate { };
+
     public void ShootBullet(Vector3 startPosition, Vector3 direction, WeaponType weaponType)
     {
         // raycast from start position into direction, infinite, get colliders
-        
+
         ShotReceiver shotReceiver = null;
         GameObject hitObject = null;
-        
-       if(Physics.Raycast(startPosition, direction, out RaycastHit hit, Mathf.Infinity))
-       {
-           hitObject = hit.collider.gameObject;
-           // Try get is a little bit slower than geting a component that exists, but a lot faster than getting one that doesn't exist
-           shotReceiver = hit.collider.TryGetComponent(out shotReceiver) ? shotReceiver : null;
-           
-       }
-        
-       Debug.Log("Fired a shot with  " + weaponType + " and hit " + hitObject + " which has a shot receiver of " + shotReceiver);
-        
-    }
 
+        if (Physics.Raycast(startPosition, direction, out RaycastHit hit, Mathf.Infinity))
+        {
+            hitObject = hit.collider.gameObject;
+            // Try get is a little bit slower than geting a component that exists, but a lot faster than getting one that doesn't exist
+            shotReceiver = hit.collider.TryGetComponent(out shotReceiver) ? shotReceiver : null;
+
+            OnShootBulletStartToTarget(startPosition, hit.point);
+        }
+        else
+        {
+            OnShootBulletStartToInfiniteDirection(startPosition, direction);
+        }
+
+        
+        Debug.Log("Fired a shot with  " + weaponType + " and hit " + hitObject + " which has a shot receiver of " + shotReceiver);
+    }
 }
---
+++ Assets/Scripts/VFX/BulletImpacts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using MinMaxSlider;
+using UnityEngine;
+
+
+public class BulletImpacts : MonoBehaviour
+{
+
+    
+    // listen to shooter
+    // allright, change of plans, just make sure that if something was hit that wasn't shootable, a bullet impact will be spawned. use th
+    
+    // get start and target positions
+    // spawn and align the object in between if it has a minimum distance.
+    // could also just show the trail somewhat closer to the actual bullet, make that a setting with variance.
+    // bullet should move real fast real quick, try out settings.
+
+ 
+    
+    [SerializeField] GameObject bulletTrailPrefab;
+
+    [SerializeField] float bulletTime;
+
+    void OnEnable()
+    {
+        Shooter.OnShootBulletStartToTarget += SpawnBulletTrailToTarget;
+        Shooter.OnShootBulletStartToInfiniteDirection += SpawnBulletTrailToInfiniteDirection;
+    }
+    
+    void OnDisable()
+    {
+        Shooter.OnShootBulletStartToTarget -= SpawnBulletTrailToTarget;
+        Shooter.OnShootBulletStartToInfiniteDirection -= SpawnBulletTrailToInfiniteDirection;
+    }
+    
+    
+
+    void SpawnBulletTrailToTarget(Vector3 arg1, Vector3 arg2)
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+    void SpawnBulletTrailToInfiniteDirection(Vector3 arg1, Vector3 arg2)
+    {
+        
+    }
+
+
+    void SpawnBulletTrail(Vector3 start, Vector3 target)
+    {
+        
+    }
+}
