e36a576fddf6aa7bc1182df1dd45236a08e6518a
Julian <julianpieper@gmx.de>
2024-01-17 12:55:03 +0100

    pop up points for kill

--- Assets/Scripts/Enemies/PopUp.cs
+++ Assets/Scripts/Enemies/PopUp.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 
 
@@ -9,13 +12,89 @@ public class PopUp : MonoBehaviour
     // grow and disappear...
     // problem is right now they live longer than enemy.
     // do it like with explosions, use same event
-    
-    
+
+    [SerializeField] float originalScale;
+    [SerializeField] Color originalColor;
+    [SerializeField] float distanceForOriginalScale;
+    [SerializeField] float reduceScaleFactorLargeDistance = 0.8f;
+
+    [SerializeField] float reducedScaleOnSpawning = 0.5f;
+    [SerializeField] float timeToReachFullScale = 1f;
+    [SerializeField] float timeToFade = 2f;
+
+    PopUpManager popUpManager;
+
+    [SerializeField] TextMeshProUGUI infoText;
     // depth needs percentage when killed
     // and extra method for tutorial to update
     // probably best to have a script for that, can use pop up prefab, set position and value every frame
     // for depth drone use two! one immediatly when shot, the other one like everything else on explosion
-    
 
 
+    // grow a bit, then fade and return to pool
+    // make it a parameter to differntiate the pop up for accuracy = grow super fast, stay, fade quickly. Should by synced to the drone lasers
+    // 
+
+
+    public void Initialize(PopUpManager pm)
+    {
+        popUpManager = pm;
+    }
+
+
+    float adjustedTargetScale;
+
+    public void SetText(string toString, float distanceToPlayer)
+    {
+        float adjustTargetScale = 1f;
+        
+        if (distanceToPlayer > distanceForOriginalScale)
+        {
+            adjustTargetScale += ((distanceToPlayer / distanceForOriginalScale) -1f) * reduceScaleFactorLargeDistance;
+        }
+        
+
+        adjustedTargetScale = originalScale * adjustTargetScale;
+
+        infoText.text = toString;
+
+        transform.localScale = Vector3.one * reducedScaleOnSpawning * adjustedTargetScale;
+
+        gameObject.SetActive(true);
+        StartCoroutine(GrowRoutine());
+    }
+
+
+    IEnumerator GrowRoutine()
+    {
+        infoText.color = originalColor;
+        
+        float time = 0;
+        while (time < timeToReachFullScale)
+        {
+            time += Time.deltaTime;
+            float progress = time / timeToReachFullScale;
+            transform.localScale = Vector3.one * Mathf.Lerp(reducedScaleOnSpawning * adjustedTargetScale, adjustedTargetScale, progress);
+            yield return null;
+        }
+
+        time = 0;
+        while (time < timeToFade)
+        {
+            time += Time.deltaTime;
+            float progress = time / timeToFade;
+            infoText.color = Color.Lerp(originalColor, Color.clear, progress);
+            yield return null;
+        }
+
+        popUpManager.ReturnToPool(this);
+    }
+
+
+    [Button]
+    void SetOriginalScaleAndColor()
+    {
+        originalScale = transform.localScale.x;
+        originalColor = infoText.color;
+    }
 }
--- Assets/Scripts/Enemies/PopUpManager.cs
+++ Assets/Scripts/Enemies/PopUpManager.cs
@@ -1,27 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 
 public class PopUpManager : MonoBehaviour
 {
     [SerializeField] PlayerPosition playerPosition;
-    
-    [SerializeField] PopUp  popUpPrefab;
-    
+
+    [SerializeField] PopUp popUpPrefab;
+
+    [SerializeField] Vector3 offset;
+
     List<PopUp> popUpPool = new List<PopUp>();
 
+    // best tool to find offset?
+    // run in update
 
+
+    // allright, need to spawn a pop up above the drone
     void OnEnable()
     {
         MultiDrone.OnMultiDroneShot += SpawnMultiDroneAccuracyPopUp;
         ScoreController.OnPointsForKillAwarded += SpawnPointsPopUp;
     }
-    
+
     void OnDisable()
     {
         MultiDrone.OnMultiDroneShot -= SpawnMultiDroneAccuracyPopUp;
         ScoreController.OnPointsForKillAwarded -= SpawnPointsPopUp;
+    }
+
+
+    void SpawnPointsPopUp(float points, Vector3 position)
+    {
+        PopUp popUp = GetPopUp();
+        popUp.transform.position = position + offset;
+        
+        // look away from player
+        popUp.transform.LookAt(playerPosition.Position);
+        popUp.transform.Rotate(0, 180, 0);
+        
+        float distanceToPlayer = Vector3.Distance(position, playerPosition.Position);
+        
+        popUp.SetText(points.ToString("F0"), distanceToPlayer);
+    }
+
+
+    void SpawnMultiDroneAccuracyPopUp(MultiDroneHitInfo hitInfo)
+    {
+        // if (popUpPrefab == null)
+        // {
+        //     Debug.LogError("PopUp prefab is null");
+        //     return;
+        // }
+        //
+        // PopUp popUp = GetPopUp();
+        // popUp.transform.position = hitInfo.Position + offset;
+        // popUp.SetText(hitInfo.Accuracy.ToString());
+        // popUp.gameObject.SetActive(true);
+    }
+
+
+    PopUp GetPopUp()
+    {
+        if (popUpPool.Count > 0)
+        {
+            PopUp popUp = popUpPool[0];
+            popUpPool.RemoveAt(0);
+            return popUp;
+        }
+        else
+        {
+            PopUp popUp = Instantiate(popUpPrefab, transform);
+            popUp.Initialize(this);
+            return popUp;
+        }
+    }
+
+
+    public Transform testDrone;
+
+    [Button]
+    void SetOffset()
+    {
+        offset = popUpPrefab.transform.position - testDrone.position;
+    }
+
+    public bool testLookat;
+    
+    void Update()
+    {
+        if (testLookat)
+        {
+            popUpPrefab.transform.LookAt(playerPosition.Position);
+            popUpPrefab.transform.Rotate(0, 180, 0);
+        }
     }
 
     // spawn one with points when enemy destroyed, unless it was a single one with wrong weapon
@@ -29,11 +103,14 @@ public class PopUpManager : MonoBehaviour
     // pool them
 
     // can get the score multiplier from the score multi, but the tricky thing is that a wrong hit will be 0
-    
+
     // show points pop up for single shot when it explodes, grab the score multi before it increases
     // show a percentage pop up whenever a multidrone gets hit, make it quick so it will be gone before drone explodes
     // show points pop up when multidrone gets destroyed. In both cases can use explosion, but need the extra info if it was correct weapon
     // and for multi drone shot need extra info of position
-    
-
+    public void ReturnToPool(PopUp popUp)
+    {
+        popUpPool.Add(popUp);
+        popUp.gameObject.SetActive(false);
+    }
 }
